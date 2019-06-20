08eb01fafc8937a1d8bb5b8e86f934493fac1141
Mauricio Barbosa <mauricio.coelho@etec.sp.gov.br>
2019-06-20 16:38:33 -0300

    Implemented socket connection event trigger, added Newtonsoft.Json dependency to handle JSON data

--- CSharpWebSocket.cs
+++ CSharpWebSocket.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Sockets;
+using Newtonsoft.Json;
 
 namespace CSharpWS2 {
     public class CSharpWebSocket {
         public IPAddress IPAddress { get; private set; }
         public int Port { get; private set; }
         public int BackLog { get; private set; }
+        private Dictionary<Guid, Client> Sockets { get; set; }
 
+        public delegate void OnClientConnect(Client client);
+        public event OnClientConnect OnClientConnectEventHandler;
+
+        private Listener serverListener;
 
         public CSharpWebSocket(IPAddress ipAddress, int port, int backLog) {
             IPAddress = ipAddress;
@@ -17,6 +25,13 @@ namespace CSharpWS2 {
         public void Listen() {
 
         }
+
+        public Client GetSocket(Guid socketGuid) {
+            Client client;
+            bool socketExists = Sockets.TryGetValue(socketGuid, out client);
+            return (socketExists ? client : new Client());
+        }
+
 
     }
 }
--- Listener.cs
+++ Listener.cs
@@ -6,15 +6,13 @@ namespace CSharpWS2 {
     public class Listener {
 
         public delegate void OnSocketConnect(Socket socket);
-        public delegate void OnSocketDisconnect(Socket socket);
-
         public event OnSocketConnect OnSocketConnectEventHandler;
-        public event OnSocketDisconnect OnSocketDisconnectEventHandler;
+        public bool IsListening { get; private set; }
 
         private IPEndPoint IPEndPoint { get; set; }
         private int BackLog { get; set; }
         private Socket Socket { get; set; }
-        public bool IsListening { get; private set; }
+
 
         public Listener(IPEndPoint ipEndPoint, int backLog) {
             IPEndPoint = ipEndPoint;
@@ -25,17 +23,19 @@ namespace CSharpWS2 {
         public void StartServer() {
             Socket.Bind(IPEndPoint);
             Socket.Listen(BackLog);
-            Socket.BeginAccept(AcceptCallback, null);
+            Socket.BeginAccept(new AsyncCallback(AcceptCallback), Socket);
             IsListening = true;
         }
 
         private void AcceptCallback(IAsyncResult asyncResult) {
             try {
                 Socket connectedSocket = Socket.EndAccept(asyncResult);
-
-
-
+                if (connectedSocket.Connected) {
+                    OnSocketConnectEventHandler(connectedSocket);
+                }
+                Socket.BeginAccept(AcceptCallback, Socket);
             } catch(Exception ex) {
+                Socket.Shutdown(SocketShutdown.Both);
                 Socket.Close();
                 Socket.Dispose();
                 IsListening = false;
